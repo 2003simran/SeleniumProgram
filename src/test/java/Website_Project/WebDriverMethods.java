157fd1ae03dc64fdddc433b69325a953fbe29748
simran <2003simran26@gmail.com>
2025-08-18 22:27:53 +0530

    complted 7 webdriver methods

--- src/test/java/Website_Project/WebDriverMethods.java
+++ src/test/java/Website_Project/WebDriverMethods.java
@@ -1,24 +1,26 @@
 package Website_Project;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverMethods {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		//webdriver is a core interface of selenium and they have 11 abstract methods+2 more methods.
 		ChromeDriver driver = new ChromeDriver();
 		//1.get(this helps to navigate to particular website and wait to load)
 		 driver.get("https://www.toolsqa.com/");
 		//2.getTilte(It is used to fetch the current page title)
 		String title = driver.getTitle();
-		System.out.println(title);
+		System.out.println("Title of the page"+title);
 		//3.getCurrentUrl(It is used to fetch url of the webpage)
 		String url = driver.getCurrentUrl();
-		System.out.println(url);
+		System.out.println("Url of the website"+url);
 		//4.getPagesource(It is used to fetch source code of the webpage)
-				String Src = driver.getPageSource();
-				System.out.println(Src);
-		//navigate(It is used to perform different operations such as backward,forward,refresh,wait....etc in browsers history)
+//				String Src = driver.getPageSource();
+//				System.out.println(Src);
+		//5.navigate(It is used to perform different operations such as backward,forward,refresh,wait....etc in browsers history)
 				//driver.navigate().forward();
 				//driver.navigate().back();
 				driver.navigate().refresh();
@@ -30,11 +32,26 @@ public class WebDriverMethods {
 				driver.manage().window().minimize();
 				//5.3(minimize the window)
 				driver.manage().window().fullscreen();
-				//5.44(get and set position and size)
-				
-		//12.close(it is used to close the current tab/window where aur control is present)
+				Thread.sleep(4000);
+				//5.4(get and set position and size)
+					//1.using get we fetch the position and size
+						Point getpos = driver.manage().window().getPosition();
+						System.out.println("postion of the screen"+getpos);
+						System.out.println(" X postion of the screen"+getpos.getX());
+						System.out.println(" X postion of the screen"+getpos.getY());
+						Dimension getsize = driver.manage().window().getSize();
+						System.out.println("size of the screen"+getsize);
+						System.out.println(" hight of the screen"+getsize.getHeight());
+						System.out.println(" width of the screen"+getsize.getWidth());
+					//2.using set we set the position and size
+						 Thread.sleep(3000);
+					  driver.manage().window().setPosition(new Point(200, 500));
+					  Thread.sleep(3000);
+					  driver.manage().window().setSize(new Dimension(400, 500));
+					  Thread.sleep(3000);
+		//7.close(it is used to close the current tab/window where aur control is present)
 				//driver.close();
-		//13.quit(it is used to close the browser)
+		//8.quit(it is used to close the browser)
 		driver.quit();
 
 	}
